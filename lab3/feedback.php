00277917784742d3fab5f011d3e80be4ce793de8
Fedorlol <borisiuk.fiodor@gmail.com>
2020-06-11 14:06:33 +0300

    Add files via upload

---
+++ lab3/feedback.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'vendor/autoload.php';
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader, array(
+    'cache'       => 'compilation_cache',
+    'auto_reload' => true
+));
+
+$books = array();
+
+echo $twig->render('feedback.html', array('books' => $books));
